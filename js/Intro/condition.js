3aa84782df3873f5d2260ee426f0bc9746f4cbbd
jaymoh34 <js6060860@gmail.com>
2026-06-24 10:03:24 +0300

    if statement

---
+++ js/Intro/condition.js
@@ -0,0 +1,46 @@
+/*
+
+
+
+
+
+Syntax:
+if you want to use if
+if(<condition goes>)
+Golden Rule -> if th condition is truthy th
+then it will execute code in /{}/
+
+*/ 
+
+//Scenario 1
+let name="James"
+if (name){
+    console.log("Scenario 1 it run")
+}
+
+//Scenario 2
+let count=0
+if (count){
+    console.log("Scenario 2 it run")
+}
+
+//Scenario 3
+if(true){
+    console.log("Scenario 3 it run")
+}
+
+//Scenario 4
+if(false){
+    console.log("Scenario 4 it run")
+}
+
+//Scenario 5
+if(10>20){
+    console.log("Scenario 5 it run")
+}
+
+//Scenario 6
+if(20>10){
+    console.log("Scenario 6 it run")
+}
+
